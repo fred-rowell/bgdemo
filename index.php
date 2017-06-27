807b99b36d70baf53bbece354ed955b74df8b104
David La Motta <david@emitrom.com>
2017-06-27 10:58:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -90,9 +90,9 @@
           </table>
         </div>
         <div class="col-md-6 col-md-offset-3">
-          <h2>Roses are red.</h2>
+          <h2>Blue roses are pretty.</h2>
           <p>
-            <img src="redrose.jpeg">
+            <img src="bluerose.jpeg">
           </p>
         </div>
       </div>
